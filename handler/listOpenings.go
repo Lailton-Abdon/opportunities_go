bd1f6a7328177c5e0cc7c1e3b0b5ac8aca60ea3b
Lailton-Abdon <lailton.abdon@gmail.com>
2024-08-14 14:39:57 -0300

    Correções e Implementações

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Lailton-Abdon/opportunities_go/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpenings_handler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(ctx, "list-openings", openings)
 }
